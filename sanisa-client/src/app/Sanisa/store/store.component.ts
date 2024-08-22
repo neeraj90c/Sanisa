1252012af471b37b5d42f93497a8f68188ee5340
shraddhav <shraddhavedre606@gmail.com>
2024-08-22 11:01:22 +0530

    Quick view code

--- sanisa-client/src/app/Sanisa/store/store.component.ts
+++ sanisa-client/src/app/Sanisa/store/store.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-store',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent {
+  @ViewChild('quickView', { static: false }) quickViewModalContent!: ElementRef;
+  quickViewModal!: NgbModalRef;
+  public modalService = inject(NgbModal)
+  OpenquickViewModal() {
+    console.log('enter');
+    this.quickViewModal = this.modalService.open(this.quickViewModalContent, { size: 'lg' });
+  }
+  
+  disabled = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+
+  formatLabel(value: number): string {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return `${value}`;
+  }
 
 }
